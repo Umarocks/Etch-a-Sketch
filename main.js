31fd7da269d2d69e6b67be6bdcc07cc5051aa6aa
Umarocks <umarocks@hotmail.com>
2023-09-15 18:27:46 -0500

    made dynamic creation of divs. have some bugs like divs overflowing its parent container.

--- main.js
+++ main.js
@@ -2,6 +2,30 @@ addEventListener("DOMContentLoaded", function () {
   var slider = document.getElementById("slider");
   var sliderValue = document.getElementById("sliderValue");
 
+  function makedivs(value) {
+    const gridDiv = document.querySelector(".grid");
+    for (i = 0; i < value; i++) {
+      for (j = 0; j < value; j++) {
+        const pixelDivCreation = document.createElement("div");
+        pixelDivCreation.classList.add("pixel");
+        gridDiv.appendChild(pixelDivCreation);
+      }
+    }
+
+    gridDiv.style.gridTemplateRows = "repeat(" + value + ", 1fr)";
+    gridDiv.style.gridTemplateColumns = "repeat(" + value + ", 1fr)";
+  }
+
+  function deleteDivs() {
+    var container = document.querySelector('.grid');
+    
+    var pixelDivs = container.querySelectorAll(".grid .pixel");
+    // Loop through the childDivs and remove each one
+    pixelDivs.forEach(function (div) {
+      div.remove();
+    });
+  }
+  
   function updateSliderValue() {
     sliderValue.textContent = slider.value + "x" + slider.value;
   }
@@ -9,10 +33,15 @@ addEventListener("DOMContentLoaded", function () {
   slider.addEventListener("input", function () {
     updateSliderValue();
     var sliderInputValue = slider.value;
-    console.log("Slider value: " + sliderInputValue);
+
     updateSliderValue();
   });
-
-
-  
+  const applyButton = document.querySelector(".apply");
+  applyButton.addEventListener("click", function () {
+    makedivs(slider.value);
+  });
+  const resetButton = document.querySelector(".reset");
+  resetButton.addEventListener("click", function () {
+    deleteDivs();
+  });
 });
